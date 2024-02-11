723d32ea49f72f83964f3139329ac653da287bbc
Abdulbosit <tohtasinov200500@gmail.com>
2024-02-11 15:47:01 +0600

    fixed bags

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { Box } from "@mui/material";
-import React from "react";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Support from "./components/Support/Support";
-import Appartments from "./components/Appartments/Appartments";
-import Contacts from "./components/Contacts/Contacts";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./MainLayout/MainLayout";
 
 function App() {
   return (
-    <Box>
-      <Header />
-      <Appartments />
-      <Support />
-      <Contacts />
-      <Footer />
-    </Box>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
+import Header from "../components/Header/Header";
+import Appartments from "../components/Appartments/Appartments";
+import Footer from "../components/Footer/Footer";
+import Support from "../components/Support/Support";
+import Contacts from "../components/Contacts/Contacts";
+
+const MainLayout = (props) => {
+  return (
+    <Box>
+      <Header />
+      <Appartments />
+      <Support />
+      <Contacts />
+      <Footer />
+    </Box>
+  );
+};
+
+MainLayout.propTypes = {};
+
+export default MainLayout;
